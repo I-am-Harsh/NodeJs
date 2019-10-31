c2a483f4d6a8c6d2be47e834a05a16500c21bd96
Harsh Verma <verma.harsh98@gmail.com>
2019-11-01 00:54:08 +0530

    mongoose updated

--- mongoose/index.js
+++ mongoose/index.js
@@ -4,32 +4,55 @@ const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/confusion';
 
-const connect = mongoose.connect(url ,{useNewUrlParser: true, useUnifiedTopology: true});
+// set these at the start to avoid depraction warning
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
-connect.then((db) =>{
+// the below method can also be used to remove the depraction warning removal.
+// const connect = mongoose.connect(url ,{useNewUrlParser: true, useUnifiedTopology: true});
+
+const connect = mongoose.connect(url);
+connect.then(() =>{
     console.log("Server is connected");
+    // Dishes.remove();
 
-    var newDish = Dishes({
-        name : "Utthapizza",
-        description : "Test desc"
-    });
-
-    // saves the values and return
-    newDish.save()
-    .then((dish) =>{
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+    .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) =>{
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return Dishes.deleteOne({});
     })
-    .then(() =>{
-        mongoose.connection.close();
+    .then(() => {
+        return mongoose.connection.close();
     })
-    .catch((err) =>{
+    .catch((err) => {
         console.log(err);
-    })
+    });
 });
